ae9ad665ec4eeb937fa45f1c45bc9f46d76700c5
Christian Solis <46233361+Tacoholic@users.noreply.github.com>
2025-12-07 22:49:59 -0600

    Added comments
    Added some comments to the auth.fixture.ts file.

--- tests/fixtures/auth.fixtures.ts
+++ tests/fixtures/auth.fixtures.ts
@@ -1,10 +1,23 @@
 import { test as base } from "@playwright/test";
 import { AuthPage } from "../pages/auth.page";
 
+/**
+ * Creating new test object 
+ * with additional fields
+ */
 export const test = base.extend<{
+  /**
+   * Every test now has an object called auth
+   * and it is of the type AuthPage
+   */
   authPage: AuthPage;
 }>({
+  /*
+   * Playwright is injecting regular
+   * Playwright fixtures with ({page}).
+   */
   authPage: async ({ page }, use) => {
+    //Page object is created and pass the actual Playwright page
     await use(new AuthPage(page));
   },
 });
